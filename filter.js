7589d5ccb0e20ae90ac9d74f4de28d21a3acc432
Mao Li <mal131@ucsd.edu>
2019-03-01 00:49:17 -0800

    implement a naive approach for filter

---
+++ filter.js
@@ -0,0 +1,28 @@
+// Load the json data
+const json = require("./toFind.json")
+const arr = json['data']
+
+function callback (fiveX) {
+    return function (value, index, array) {
+        return value["FiveX"].includes(fiveX);
+    }
+}
+
+var object = {
+    "data": {
+        "fire": arr.filter(callback("fire")),
+        "water": arr.filter(callback("water")),
+        "gold": arr.filter(callback("gold")),
+        "wood": arr.filter(callback("wood")),
+        "dirt": arr.filter(callback("dirt"))
+    }
+}
+
+var fs = require('fs')
+var to_write = JSON.stringify(object)
+fs.writeFile('my_filter_ans.json', to_write, 'utf8', function(err) {
+    if (err) throw err;
+    console.log('complete');
+    });
+
+
